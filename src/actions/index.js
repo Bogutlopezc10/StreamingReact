c14d0581ef7f29a722d66edf5775959324f15a58
Camilo Lopez <camilopezc@hotmail.com>
2020-01-28 09:36:17 -0500

    App Start

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,27 +1,12 @@
 import streams from '../apis/streams';
 import history from '../history';
 import { 
-    SIGN_IN, 
-    SIGN_OUT, 
     CREATE_STREAM,
     FETCH_STREAMS,
     FETCH_STREAM,
     DELETE_STREAM,
     EDIT_STREAM
 } from './types';
-
-export const signIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    };
-};
-
-export const signOut = () => {
-    return {
-        type: SIGN_OUT
-    };
-};
 
 export const createStream = (formValues) => async (dispatch, getState) => {
     const { userId } = getState().auth;
