cc39635cdd7abbf117663a97d3c7c630dc9ce0d5
juan carlos gonzales montes <pope624@hotmail.com>
2020-01-30 10:30:48 -0500

    Adding Course Page

---
+++ src/reducers/courseReducer.js
@@ -0,0 +1,34 @@
+import _ from 'lodash';
+import {
+    CREATE_CATERGORY,
+    FETCH_COURSES,
+    FETCH_CATEGORY,
+    EDIT_CATEGORY,
+    DELETE_CATEGORY
+} from '../actions/types';
+
+// export default (state={}, action) => {
+//     switch (action.type){
+//         case CREATE_CATERGORY:
+//             return { ...state, ..._.mapKeys(action.payload,'id')};
+//         case FETCH_CATEGORIES:
+//             return { ...state, [action.payload.id]: action.payload };
+//         case CREATE_STREAM:
+//             return { ...state, [action.payload.id]: action.payload };
+//         case EDIT_STREAM:
+//             return { ...state, [action.payload.id]: action.payload };
+//         case DELETE_STREAM:
+//             return _.omit(state, action.payload);
+//         default:
+//             return state;
+//     }
+// }
+
+export default (state={}, action) => {
+    switch (action.type){
+        case FETCH_COURSES:
+            return { ...state, ..._.mapKeys(action.payload,'id')};
+        default:
+            return state;
+    }
+}
