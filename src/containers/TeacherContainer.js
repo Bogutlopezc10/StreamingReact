8635592edb880bdeacb5c1b17c74c6045389d04c
Camilo Lopez <camilopezc@hotmail.com>
2020-02-01 09:40:52 -0500

    Partial TeacherPage Design

--- src/containers/TeacherContainer.js
+++ src/containers/TeacherContainer.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import CoursePublished from '../components/courses/teacher/CoursePublished';
+import './TeacherContainer.css'
 import { connect } from 'react-redux';
 
 class TeacherContainer extends React.Component{
     render(){
         return (
-            <div>
-                <CoursePublished />
-            </div>
-        )
+            <>
+                <div className="teacher-title d-flex align-items-center justify-content-center mb-4">
+                    <h1>Panel de control</h1>
+                </div>
+                <div className="container mb-2">
+                    <div className="row">
+                        <div className="col-lg-12 d-flex justify-content-start">
+                            <h1 className="title-published">Mis cursos publicados</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className="container container-teacher pt-4 px-4 mb-5 testimonial-group">
+                    <div className="row text-center">
+                        <CoursePublished />
+                    </div>
+                </div>
+            </>
+        );
     }
 }
 
