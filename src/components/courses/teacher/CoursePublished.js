df3e8259905d5fd2d0a2fc3bf4a6b11aac66bd3d
Camilo Lopez <camilopezc@hotmail.com>
2020-02-01 11:10:14 -0500

    Design Teacher PublishedCourse

--- src/components/courses/teacher/CoursePublished.js
+++ src/components/courses/teacher/CoursePublished.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import './Teacher.css';
+import { Link } from 'react-router-dom';
+import '../CourseList.css'
 
-const CoursePublished = (props) => {
-
-    console.log(props.courses)
+const CoursePublished = ({ courses }) => {
     return (
-        <>
-            
-        </>
-    )
+        courses.map( course => {
+            const starPercentage = (course.rating / 5) * 100;
+            const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
+            return(
+                <div key={course.id} className="col-lg-3 mb-4">
+                    <div className="card shadow h-100">
+                        <img src="/bg-home.jpg" className="card-img" width="150" height="150" alt="bg-home" />
+                        <div className="card-body">
+                            <h5 className="card-title">{course.name}</h5>
+                        </div>
+                        <div className="card-footer">
+                            <div className="row">
+                                <div className="col-lg-12 d-flex align-items-center justify-content-center">
+                                    <div className="stars-outer">
+                                        <div className="stars-inner" style={{ width: starPercentageRounded }}></div>
+                                    </div>
+                                    <span className="number-rating ml-2 text-muted">{course.rating}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card-badge shadow rounded-right text-uppercase">{course.categoryName}</div>
+                        <Link to={`courses/`} className="btn course-button">
+                            <div>
+                                <p className="d-inline">VER DETALLE</p> 
+                                <i className="d-inline fas fa-angle-double-right ml-2 mt-2"></i>
+                            </div>
+                        </Link>
+                    </div>
+                    
+                </div>
+            )
+        })
+    );
 }
 
 export default CoursePublished;
