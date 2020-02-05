cb7155cc69dc4a0e9cb15da1aaca7c99d381ea3e
juan carlos gonzales montes <pope624@hotmail.com>
2020-02-04 19:16:08 -0500

    Redirect to Home  when state is false in the error

--- src/containers/ErrorContainer.js
+++ src/containers/ErrorContainer.js
@@ -3,12 +3,16 @@ import Error from '../components/Error'
 import MainHeader from '../components/MainHeader'
 import { connect } from 'react-redux';
 import {updateErrorUnmount} from '../actions/error'
+import history from '../history'
 class ErrorContainer extends React.Component {
 
     componentWillUnmount(){
         this.props.updateErrorUnmount()
     }
     render(){
+        if(!this.props.error.isError){
+            history.push('/');
+        }
         return(
             <>
                 <MainHeader backgroundHeaderColor="#005385" textHeader="Error" />
