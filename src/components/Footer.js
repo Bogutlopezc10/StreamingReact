9496069dae5a535711a4e1f95c937ac1d6bb16a0
Camilo Lopez <camilopezc@hotmail.com>
2020-03-11 11:44:48 -0500

    Footer links.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,19 @@ const Footer = () => {
                     <div className="container">
                         <div className="row d-flex social-network mt-2 mb-3 justify-content-center">
                             <div className="col-auto">
-                                <i className="fab fa-facebook"></i>
+                                <a href="https://www.facebook.com/juancarlos.gonzalesmontes?ref=bookmarks">
+                                    <i className="fab fa-facebook"></i>
+                                </a>
                             </div>
                             <div className="col-auto">
-                                <i className="fab fa-twitter"></i>
+                                <a href="https://twitter.com/juan9607">
+                                    <i className="fab fa-twitter"></i>
+                                </a>
                             </div>
                             <div className="col-auto">
-                                <i className="fab fa-github"></i>
+                                <a href="https://github.com/Bogutlopezc10">
+                                    <i className="fab fa-github"></i>
+                                </a>    
                             </div>
                         </div>
                         <div className="row">
